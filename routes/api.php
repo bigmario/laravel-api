4ea2df9e4e30988c77d5c73678264b307b09595f
Mario Castro <mariocastro.pva@gmail.com>
2022-06-02 11:07:33 -0400

    initial commit

---
+++ routes/api.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\Http\Controllers\Api\LoginController;
+use App\Http\Controllers\Api\v1\PostController as PostV1;
+use App\Http\Controllers\Api\v2\PostController as PostV2;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+
+// V1
+Route::apiResource('v1/posts', PostV1::class)
+    ->only(['index', 'show', 'destroy'])
+    ->middleware('auth:sanctum');
+
+// V2
+Route::apiResource('v2/posts', PostV2::class)
+    ->only(['index', 'show'])
+    ->middleware('auth:sanctum');
+
+//LOGIN
+Route::post('login', [
+    LoginController::class,
+    'login'
+]);
